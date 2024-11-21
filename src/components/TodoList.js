806dc1b0b61718d5db80c867e5a174a5b877673d
kevinfrayed-knot <kevin@frayed-knot.com>
2024-11-21 15:03:31 -0600

    updated todo list component with task manger inout feilds

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,8 +4,31 @@ function TodoList(props) {
     return (
       <>
         <h4>{props.title}</h4>
+        <div>
+          {"item [   ] add reset"}
+        </div>
+        <div>
+          <p>{" msg "}</p>
+        </div>
+        <div>
+          {" all | completed | in-completd"}
+        </div>
+        <p></p>
+        <div>
+          <p>
+            {"[x] item one [x]"}
+          </p>
+          <p>
+            {"[] item two [x]"}
+          </p>
+          <p>
+            {"[x] item three [x]"}
+          </p>
+          <p>
+            {"[] item four [x]"}
+          </p>
+        </div>
       </>
     );
   }
-  
   export default TodoList;
